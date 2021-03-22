754c924f4f5ff0cf0b660c5a2d656727edf83c73
Aritra Sengupta <aritra55@gmail.com>
2021-03-22 08:12:43 +0530

    adding build folder for github pages

--- src/components/PhutBall.js
+++ src/components/PhutBall.js
@@ -177,8 +177,9 @@ export default class PhutBall extends React.Component {
           <ul>
             <li>Single click on an empty grid to create a player (1 turn)</li>
             <li>Drag the ball (yellow dot) to a free grid provided there are players to jump over</li>
-            <li>In one turn multiple jumps can be perfomed provided they are legal. Jumps are optional and can be stopped at any time.</li>
+            <li>In one turn multiple jumps can be performed provided they are legal. Jumps are optional and can be stopped at any time.</li>
             <li>Goal is to drag the ball to the opponents goal</li>
+            <li>Before a game begins switch to auto or manual will make the game versus computer or another player. Resets after every goal</li>
           </ul>
 
           <span style={{ fontSize: '15px', fontWeight: 500 }}>UI:</span>
@@ -186,7 +187,7 @@ export default class PhutBall extends React.Component {
             <li>Ball: yellow, players: black and transparent circles to show possible ball position in the next move</li>
             <li>Drag the ball and it will show the possible positions</li>
             <li>Blue: neutral position, green: winning position, red: own goal(losing position)</li>
-            <li>The game will calculate if the player has already just before the final move and need not be perfomed manually</li>
+            <li>The game will calculate if the player has already just before the final move and need not be performed manually</li>
           </ul>
         </div>}
         <div style={{flex: 1, width: '90%', borderBottom: '2px solid black', marginBottom: '10px'}}>
